6eb6542e56c5d1f5ed8a88dc3a879a5c48392750
Kate Byars and Shyamal Punekar <kate@>
2017-08-01 09:51:55 -0700

    Add first unit test

---
+++ src/main/java/models/Scrabble.java
@@ -0,0 +1,17 @@
+package models;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Guest on 8/1/17.
+ */
+public class Scrabble {
+
+    public Integer calculateScore(String input){
+        Map<Character, Integer> scoreVals = new HashMap();
+
+        return null;
+    }
+
+}
---
+++ src/test/java/models/ScrabbleTest.java
@@ -0,0 +1,19 @@
+package models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Guest on 8/1/17.
+ */
+public class ScrabbleTest {
+
+    @Test
+    public void calculateScore_returnsScoreForSingleLetter_1() throws Exception {
+        Scrabble testScrabble = new Scrabble();
+        Integer expected = 1;
+        assertEquals(expected, testScrabble.calculateScore("a"));
+    }
+
+}
